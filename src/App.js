ced266adb3db14e6e50e98bbea1a4fc0455e59f8
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-03-18 19:56:27 +0330

    added tasks to App level

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appoinment",
+      day: "Feb 5th at 2:30pm",
+      remider: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      remider: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      remider: false,
+    },
+  ]);
   return (
     <div className="App">
       <div className="container border mt-4 shadow-sm rounded">
         <Header />
-        <Tasks />
+        <Tasks tasks={tasks} />
       </div>
     </div>
   );
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,4 @@
-const Tasks = () => {
-  const tasks = [
-    {
-      id: 1,
-      text: "Doctors Appoinment",
-      day: "Feb 5th at 2:30pm",
-      remider: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      remider: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "Feb 5th at 2:30pm",
-      remider: false,
-    },
-  ];
+const Tasks = ({ tasks }) => {
   return (
     <div className="Tasks">
       <div className=" px-5 py-5">
