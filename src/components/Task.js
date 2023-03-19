613ea3dffd320696fd627a4a183fdd641b1d38a0
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-03-19 21:11:02 +0330

    added react-icon and close button

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,18 @@
+import React from "react";
+import { FaTimes } from "react-icons/fa";
 const Task = ({ task }) => {
   return (
     <div className="Task">
-      <div className="border mb-3 px-4 py-1 mx-4 shadow-sm">
-        <h4>{task.text}</h4>
-        <p className="mb-1">{task.day}</p>
+      <div class="d-flex border mb-3 px-4 py-1 mx-4 shadow-sm justify-content-between">
+        <div className="">
+          <h4>{task.text}</h4>
+          <p className="mb-1">{task.day}</p>
+        </div>
+        <div className="mt-3">
+          <a href="/" className="pe-auto">
+            <FaTimes color="red" />
+          </a>
+        </div>
       </div>
     </div>
   );
