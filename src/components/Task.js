d59561e4a326113f2dd4571c49ad2175abb10749
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-03-19 21:11:56 +0330

    rounded border around tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ import { FaTimes } from "react-icons/fa";
 const Task = ({ task }) => {
   return (
     <div className="Task">
-      <div class="d-flex border mb-3 px-4 py-1 mx-4 shadow-sm justify-content-between">
+      <div class="d-flex border rounded mb-3 px-4 py-1 mx-4 shadow-sm justify-content-between">
         <div className="">
           <h4>{task.text}</h4>
           <p className="mb-1">{task.day}</p>
