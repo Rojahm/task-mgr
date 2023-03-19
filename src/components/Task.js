cf560641a1f4840edb9d3c6f16d558e787b653e3
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-03-19 20:23:25 +0330

    added task day info

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,6 +3,7 @@ const Task = ({ task }) => {
     <div className="Task">
       <div className="border mb-3 px-4 py-1 mx-4 shadow-sm">
         <h4>{task.text}</h4>
+        <p className="mb-1">{task.day}</p>
       </div>
     </div>
   );
